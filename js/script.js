fd2dbc3a9aeac82da53a03cbb95a897972ff031c
Lawrence segun <lawrencesegun025@gmail.com>
2022-07-07 15:31:37 -0600

    data section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 (function () {
     let introduction_content = document.querySelector(".introduction_content")
+    let data_caption_para = document.querySelector(".data_caption p")
     var options = {
         threshold: 0.5
     }
@@ -12,6 +13,7 @@
             }
         })
     }, options)
+    slideFromBottom.observe(data_caption_para)
     slideFromBottom.observe(introduction_content)
 })()
 
@@ -25,3 +27,44 @@ document.addEventListener("DOMContentLoaded", function () {
     headerh3.classList.add("fadeIn")
     caption.classList.add("fadeIn")
 })
+
+function slideX(){
+    let data_captionh1 = document.querySelector(".data_caption h1")
+    let data_captionh3 = document.querySelector(".data_caption h3")
+    var options = {
+        threshold: 0.5
+    }
+    const slideX = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("slideX")
+            } else {
+                entry.target.classList.remove("slideX")
+            }
+        })
+    }, options)
+    slideX.observe(data_captionh1)
+    slideX.observe(data_captionh3)
+
+}
+
+slideX()
+
+function animatePng() {
+    let halfPng = document.querySelector(".half-png")
+    var options = {
+        threshold: 0.1
+    }
+    const rotatePng = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("rotatePng")
+            } else {
+                entry.target.classList.remove("rotatePng")
+            }
+        })
+    }, options)
+    rotatePng.observe(halfPng)
+}
+
+animatePng()
